8ee7e8501c6cc91a3d62baa8283904dd578daa4e
Khalid Jameer <kjameer0@gmail.com>
2023-10-22 13:54:29 -0400

    all middleware needs next

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const userRouter = require('./routes/users')
-const app = express()
+const express = require("express");
+const userRouter = require("./routes/users");
+const app = express();
 
-
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next()
+}
+app.use(logger);
 //view engine setter
-app.set('view engine', 'ejs')
-app.get('/', (req, res) => {
-  console.log('hi')
-  res.render('index')
-})
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  console.log("hi");
+  res.render("index");
+});
 
-app.use('/users', userRouter)
+app.use("/users", userRouter);
 
-app.listen(3000)
+app.listen(3000);
