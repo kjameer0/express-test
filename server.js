16e826d623e39f335260c54f753201ff4db8d350
Khalid Jameer <kjameer0@gmail.com>
2023-10-22 14:12:37 -0400

    express static can serve an entire folder file system

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const userRouter = require("./routes/users");
 const app = express();
 app.use(logger);
+app.use(express.static('public'))
 
 app.get("/", logger, (req, res) => {
   console.log("hik");
-  res.render("index");
+  res.render("gi");
 });
 app.set("view engine", "ejs");
 //view engine setter
