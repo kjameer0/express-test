5d39e37a7f0cabf22831646347f9e92e3fbe9a72
Khalid Jameer <kjameer0@gmail.com>
2023-10-22 13:42:20 -0400

    router.param can activate middleware to process params in url

--- routes/users.js
+++ routes/users.js
@@ -12,13 +12,19 @@ router.get("/new", (req, res) => {
 router
   .route("/:id")
   .get((req, res) => {
+    console.log(req.user);
     res.send("User id:" + req.params.id);
   })
-  .put("/:id", (req, res) => {
+  .put((req, res) => {
     res.send("Change id:" + req.params.id);
   })
-  .delete("/:id", (req, res) => {
+  .delete((req, res) => {
     res.send("Delete id:" + req.params.id);
   });
-
+//this param thing runs before the routes above
+const users = [{ name: "kyle" }, { name: "sally" }];
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
+});
 module.exports = router;
